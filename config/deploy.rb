882bcb060e8eb3cd439fe188234bd234c567e338
MasamiOno <44540758+MasamiOno@users.noreply.github.com>
2023-11-19 19:47:15 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 #set :keep_releases, 3
 
 #set :rbenv_type, :user # or :system, depends on your rbenv setup
-#set :rbenv_ruby, '2.4.1'
+#set :rbenv_ruby, '2.6.0'
 
 #set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 #set :rbenv_roles, :all # default value
@@ -10,7 +10,7 @@
 #set :linked_dirs, %w{log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle}
 #set :linked_files, %w{config/credentials.yml.enc}
 
-lock "3.11.0"
+lock "3.18.0"
 
 set :repo_url, "https://github.com/MasamiOno/ppaphotel" #自分のリポジトリURLに変更
 
@@ -26,7 +26,7 @@ set :deploy_to, '/opt/ppaphotel'
 
 #set :bundle_path, '/home/linuxbrew/.linuxbrew/bin//bundle'
 #set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-#set :rbenv_ruby, '2.4.1'
+#set :rbenv_ruby, '2.6.0'
 set :pty,             false
 set :use_sudo,        true
 set :stage,           :production
